cbcf254186bbb44269eeada33cd78db2046fb49c
Anwarhacker <patelanwar647@gmail.com>
2025-05-28 00:52:37 +0530

    added manual

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,20 @@ import React from 'react'
 import devops from './devops.pdf'
 import devops6 from './devops6.pdf'
 import devops8 from './devops8.pdf'
+import devops_manual from './devops_manual.pdf'
 
 
 const App = () => {
   return (
     <div className="min-h-screen bg-red-800">
       <div className="container mx-auto p-8 flex flex-col items-center justify-center gap-8">
+        <embed 
+          className="border-2 border-red-500 rounded-lg shadow-lg bg-white p-2"
+          src={devops_manual}
+          type="application/pdf"
+          width="1200px"
+          height="850px"
+        />
         <embed 
           className="border-2 border-red-500 rounded-lg shadow-lg bg-white p-2"
           src={devops}
